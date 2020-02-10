32edc112c8d3681035f3c33f8efca35f454bb7c7
Elias Nahas <elias.nahas@gmail.com>
2020-02-09 22:08:17 -0700

    Created Products and ProductsDB
    Created Products class and initial methods GetProducts and GetProductByID database methods.

---
+++ Team5_Workshop4/Team5_Workshop4/Products.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team5_Workshop4
+{
+    public class Products
+    {
+        public int ProductID { get; set; }
+
+        public string ProdName { get; set; }
+    }
+}
