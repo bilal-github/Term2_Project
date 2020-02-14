f4a07f926ce799bae4b080aa5d5cd23858ab00a1
Ivan Lo <ivan.lo@edu.sait.ca>
2020-02-14 10:08:12 -0700

    updated

---
+++ Team5_Workshop5/Team5_Workshop5/Models/ProductDetails.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team5_Workshop5.Models
+{
+    public class Customer
+    {
+        public int PackageId { get; set; }
+
+        public string Description { get; set; }
+
+        public string ProdName { get; set; }
+
+        public string SupName { get; set; }
+
+        public DateTime TripStart { get; set; }
+
+        public DateTime TripEnd { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ Team5_Workshop5/Team5_Workshop5/Models/TravelExpertsDB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team5_Workshop5.Models
+{
+    public static class TravelExpertsDB
+    {
+        public static string GetConnection()
+        {
+            return ConfigurationManager.ConnectionStrings["TravelExpertsConnection"].ConnectionString;
+        }
+        
+    }
+}
